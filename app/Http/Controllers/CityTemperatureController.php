298f03153ec206d7054452db464dd1333a5873f1
bodzile <155837912+bodzile@users.noreply.github.com>
2024-12-24 02:29:03 +0100

    Dodate funkcionalnost za dodavanje gradova i malo izmenjena add-city stranica

--- app/Http/Controllers/CityTemperatureController.php
+++ app/Http/Controllers/CityTemperatureController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CityTemperatureModel;
 use Illuminate\Http\Request;
 
 class CityTemperatureController extends Controller
 {
-    //
+    
+    public function addCity(Request $request)
+    {
+        $request->validate([
+            "city" => "required|unique:city_temperatures",
+            "temperature" => "required|numeric"
+        ]);
+
+        CityTemperatureModel::create([
+            "city" => $request->get("city"),
+            "temperature" => $request->get("temperature")
+        ]);
+
+        return redirect()->back();
+    }
+
 }
--- resources/views/add-city.blade.php
+++ resources/views/add-city.blade.php
@@ -7,13 +7,15 @@
         {{csrf_field()}}
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Grad</label>
-        <input type="text" name="city" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="text" name="city" class="form-control" id="exampleInputEmail1" value="{{old("city")}}" aria-describedby="emailHelp"> 
     </div>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Temperatura</label>
-        <input type="text" name="temperature" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="text" name="temperature" class="form-control" id="exampleInputEmail1" value="{{old("temperature")}}" aria-describedby="emailHelp">
     </div>
-
+    @if ($errors->any())
+        <p class="text-danger">{{$errors->first()}}</p>
+    @endif
     <button type="submit" class="mt-3 btn btn-primary">Dodaj</button>
     </form>
 @endsection
