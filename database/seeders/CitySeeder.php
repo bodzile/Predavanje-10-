e574b37213e276ce8efdc6e5f739800bb25f5e3f
bodzile <155837912+bodzile@users.noreply.github.com>
2024-12-28 22:50:29 +0100

    Added CItyModel, CitySeeder and migration for cities table

---
+++ database/seeders/CitySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CityModel;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $amount=$this->command->ask("How many cities you want to add?",100);
+        $faker=Factory::create();
+
+        $this->command->getOutput()->progressStart($amount);
+        for($i=0;$i<$amount;$i++)
+        {
+            $city=$faker->city();
+            $cityObject=CityModel::where(["name" => $city]);
+            if($cityObject) continue;
+
+            CityModel::create([
+                "name" => $city
+            ]);
+
+            $this->command->getOutput()->progressAdvance(1);
+        }
+        $this->command->info("Seeding Successfull");
+    }
+}
