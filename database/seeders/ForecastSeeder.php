b7fa33029022ef8aa227ced6244747efc9511c7c
bodzile <155837912+bodzile@users.noreply.github.com>
2024-12-28 23:52:05 +0100

    ispravljen forecastseeder, odnosno datum

--- database/seeders/ForecastSeeder.php
+++ database/seeders/ForecastSeeder.php
@@ -6,6 +6,7 @@ use App\Models\CityModel;
 use App\Models\ForecastModel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ForecastSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class ForecastSeeder extends Seeder
      */
     public function run(): void
     {
+        //DB::table('forecasts')->truncate();
         $cities=CityModel::all();
 
         $range = range(-40, 40);
@@ -21,10 +23,10 @@ class ForecastSeeder extends Seeder
         for($i=0;$i<count($cities);$i++)
         {
             $random = collect($range)->shuffle()->slice(0, 5);
-            $int= rand(1262055681,1262055681);
-            $date=date("Y-m-d H:i:s",$int);
             for($j=0;$j<5;$j++)
             {
+                $int= rand(126242352,9936242352);
+                $date=date("Y-m-d H:i:s",$int);
                 ForecastModel::create([
                     "city_id" => $cities[$i]->id,
                     "temperature" => $random[$j],
