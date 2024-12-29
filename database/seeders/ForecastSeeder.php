ee0331be2a86fd28380e2e81f796e5a3cf3a6fea
bodzile <155837912+bodzile@users.noreply.github.com>
2024-12-29 01:01:19 +0100

    Promenjen ForecastSeeder

--- database/seeders/ForecastSeeder.php
+++ database/seeders/ForecastSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\CityModel;
 use App\Models\ForecastModel;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,19 +19,14 @@ class ForecastSeeder extends Seeder
         //DB::table('forecasts')->truncate();
         $cities=CityModel::all();
 
-        $range = range(-40, 40);
-
         for($i=0;$i<count($cities);$i++)
         {
-            $random = collect($range)->shuffle()->slice(0, 5);
             for($j=0;$j<5;$j++)
             {
-                $int= rand(126242352,9936242352);
-                $date=date("Y-m-d H:i:s",$int);
                 ForecastModel::create([
                     "city_id" => $cities[$i]->id,
-                    "temperature" => $random[$j],
-                    "date" => $date
+                    "temperature" => rand(15,30),
+                    "date" => Carbon::now()->addDays(rand(1,30))
                 ]);
             }
         }
